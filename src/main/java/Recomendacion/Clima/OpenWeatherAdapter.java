b4eae65bb931aec9f19f84e99aa437d87f62206a
ignacioreyesfs <ignacioreyesfs@gmail.com>
2020-04-01 11:50:48 -0300

    Agrego forma de obtener data api usando OkHttp Library

--- src/main/java/Recomendacion/Clima/OpenWeatherAdapter.java
+++ src/main/java/Recomendacion/Clima/OpenWeatherAdapter.java
@@ -1,27 +1,37 @@
 package Recomendacion.Clima;
 
+import Recomendacion.Clima.Exceptions.MensajeErrorApiException;
 import Recomendacion.Clima.Exceptions.ProblemaConexionApiException;
-import org.json.simple.JSONArray;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class OpenWeatherAdapter implements ProveedorClima {
-    @Override
-    public double obtenerTemperaturaAhora() {
-        //TODO: ocultar mi api key
-        //Usando httpUrlConnection
+
+    //TODO: ocultar mi api key
+    private static String API_KEY = "3d8400ed6a740b8d6684b988a5d0b4d9";
+    private String urlTemperaturaAhora = "http://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,Argentina&appid=" + API_KEY;
+
+    //Usando HttpUrlConnection
+    //Usar OkHttp Library (abajo), es mas simple. Dejo este como ejemplo de HttpUrlConnection
+    public double obtenerTemperaturaAhoraHttpURlConnection() {
         HttpURLConnection con;
         URL url;
         String resultado = null;
         String temperaturaString;
         try {
-            url = new URL("http://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,Argentina&appid=3d8400ed6a740b8d6684b988a5d0b4d9");
+            url = new URL(urlTemperaturaAhora);
             con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.connect();
@@ -30,13 +40,13 @@ public class OpenWeatherAdapter implements ProveedorClima {
             if(con.getResponseCode() != 200)
                 throw new ProblemaConexionApiException();
 
-            //Leo la data como string en formato json.
+            //Paso la data de inputStream a string en formato json.
             BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
             StringBuilder sb = new StringBuilder();
-            String line;
+            String linea;
             try {
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
+                while ((linea = br.readLine()) != null) {
+                    sb.append(linea);
                 }
                 resultado = sb.toString();
                 br.close();
@@ -50,7 +60,7 @@ public class OpenWeatherAdapter implements ProveedorClima {
             throw new ProblemaConexionApiException();
         }
 
-        //Obtengo el objeto value de la key json en formato string deseado
+        //Obtengo el value de la key json en formato string
         JSONParser parse = new JSONParser();
         JSONObject jsonObject = new JSONObject();
         try {
@@ -58,14 +68,51 @@ public class OpenWeatherAdapter implements ProveedorClima {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
         //Aca fijarse como viene el JSON, si es un array usar JSONArray
         temperaturaString = ((JSONObject)jsonObject.get("main")).get("temp").toString();
 
-        return this.kelvinToCelsius(Double.parseDouble(temperaturaString));
+        return this.kelvinACelsius(Double.parseDouble(temperaturaString));
     }
 
-    private double kelvinToCelsius(double kelvin){
+    //Usando OkHttp library
+    @Override
+    public double obtenerTemperaturaAhora(){
+        double temperatura;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(urlTemperaturaAhora).build();
+        Response response;
+        String jsonBody;
+        try {
+             response = client.newCall(request).execute();
+
+             //Verifico que la api responda 200 (OK)
+            if(!response.isSuccessful())
+                throw new MensajeErrorApiException(response.code());
+
+             // este es el body del json
+             jsonBody = response.body().string();
+
+        } catch (IOException e) {
+            throw new ProblemaConexionApiException();
+        }
+
+        //obtengo el campo temperatura como string del body json
+        JSONParser parse = new JSONParser();
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = (JSONObject)parse.parse(jsonBody);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //Aca fijarse como viene el JSON, si es un array usar JSONArray
+        String temperaturaString = ((JSONObject)jsonObject.get("main")).get("temp").toString();
+
+        return kelvinACelsius(Double.parseDouble(temperaturaString));
+    }
+
+    private double kelvinACelsius(double kelvin){
         return kelvin - 273;
     }
 }
