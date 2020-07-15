9943abea4e8f6e703cbc94b905e38140129f262c
ignacioreyesfs <ignacioreyesfs@gmail.com>
2020-07-14 23:10:30 -0300

    added offerStartJob class

---
+++ src/main/java/Product/Offer/OfferStartJob.java
@@ -0,0 +1,18 @@
+package Product.Offer;
+
+import Product.Product;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import Product.Condition;
+
+public class OfferStartJob implements Job {
+    public void execute(JobExecutionContext context){
+        JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+        Condition discount = (Condition)dataMap.get("discount");
+        // TODO: when persistence is implemented, change it to product id
+        Product product = (Product)dataMap.get("product");
+
+        product.setCondition(discount);
+    }
+}
